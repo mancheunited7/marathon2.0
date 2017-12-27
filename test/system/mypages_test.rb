d05e57e991722e2be087ceab8b7de7f5ae69897b
森田啓太 <mancheunited7>
2017-12-27 18:45:23 +0900

    first

---
+++ test/system/mypages_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MypagesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit mypages_url
+  #
+  #   assert_selector "h1", text: "Mypage"
+  # end
+end
