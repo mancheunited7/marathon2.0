ec4a7f2de2d970605b77c23167e2dde0d2471e73
森田啓太 <mancheunited7>
2017-12-29 08:24:42 +0900

    login

--- db/schema.rb
+++ db/schema.rb
@@ -51,25 +51,38 @@ ActiveRecord::Schema.define(version: 20171227124857) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "userid", default: "", null: false
-    t.string "provider", default: "", null: false
-    t.string "image_url"
-    t.string "name"
-    t.string "avatar"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "uid", default: "", null: false
+    t.string "provider", default: "", null: false
+    t.string "image_url"
+    t.string "name"
+    t.string "avatar"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email", "provider", "userid"], name: "index_users_on_email_and_provider_and_userid", unique: true
+    t.index ["email", "uid", "provider"], name: "index_users_on_email_and_uid_and_provider", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
