24ee1513f61268a85d7a54931a62a6e80c4b5ca3
森田啓太 <mancheunited7>
2017-12-29 18:15:51 +0900

    snsログイン設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,66 +9,70 @@ class User < ApplicationRecord
   has_many :competitionresults
 
   def self.find_for_facebook(auth,signed_in_resource=nil)
-    user = User.find_by(email: auth.info.email, provider: auth.provider)
+    user = User.find_by(email: auth.info.email, provider: auth.provider, userid: auth.uid)
 
     unless user
       user = User.new(
-               email: auth.info.email,
+               email: auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
                provider: auth.provider,
                name: auth.extra.raw_info.name,
                userid: auth.uid,
                image_url: auth.info.image,
                password: Devise.friendly_token[0,20],
              )
-      user.save(validate: false)
-    end
-
-  end
-
-  def self.find_for_twitter(auth,signed_in_resource=nil)
-    user = User.find_by(email: auth.info.email, provider: auth.provider)
-
-    unless user
-      user = User.new(
-              email: auth.info.email ||= "#{auth.userid}-#{auth.provider}@example.com",
-              provider: auth.provider,
-              name: auth.info.nickname,
-              userid: auth.uid,
-              password: Devise.friendly_token[0,20],
-       )
+      user.skip_confirmation!
       user.save(validate: false)
     end
     user
   end
 
-  def self.find_for_google(auth,signed_in_hash=nil)
-    user = User.find_by(email: auth.info.email, provider: auth.provider)
+  def self.find_for_twitter(auth,signed_in_resource=nil)
+    user = User.find_by(email: auth.info.email, provider: auth.provider, userid: auth.uid)
 
     unless user
       user = User.new(
-              email: auth.info.email,
+              email: auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+              provider: auth.provider,
+              name: auth.info.nickname,
+              userid: auth.uid,
+              password: Devise.friendly_token[0,20],
+       )
+      user.skip_confirmation!
+      user.save
+    end
+    user
+  end
+
+  def self.find_for_google(auth,signed_in_hash=nil)
+    user = User.find_by(email: auth.info.email, provider: auth.provider, userid: auth.uid)
+
+    unless user
+      user = User.new(
+              email: auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
               provider: auth.provider,
               name: auth.info.name,
               userid: auth.uid,
               password: Devise.friendly_token[0,20],
        )
+      user.skip_confirmation!
       user.save(validate: false)
     end
     user
   end
 
   def self.find_for_instagram(auth,signed_in_hash=nil)
-    user = User.find_by(email: auth.info.email, provider: auth.provider)
+    user = User.find_by(email: auth.info.email, provider: auth.provider, userid: auth.uid)
 
     unless user
       user = User.new(
-              email: auth.info.email ||= "#{auth.userid}-#{auth.provider}@example.com",
+              email: auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
               provider: auth.provider,
               name: auth.info.name,
               userid: auth.uid,
               password: Devise.friendly_token[0,20],
              )
-      user.save(validate: false)
+      user.skip_confirmation!
+      user.save
     end
     user
   end
