856cd4dd774c236df2216aa32506fc2f5e4de463
森田啓太 <mancheunited7>
2018-01-28 14:00:37 +0900

    best_time

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,11 +2,11 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @best_time_5 = CompetitionResult.where(distance: 5).order(:calc_time).limit(1)
-    @best_time_10 = CompetitionResult.where(distance: 10).order(:calc_time).limit(1)
-    @best_time_21 = CompetitionResult.where(distance: 21).order(:calc_time).limit(1)
-    @best_time_42 = CompetitionResult.where(distance: 42).order(:calc_time).limit(1)
-    @best_time_100 = CompetitionResult.where(distance: 100).order(:calc_time).limit(1)
+    @best_time_5 = CompetitionResult.where(distance: 5).order(:calc_time).first
+    @best_time_10 = CompetitionResult.where(distance: 10).order(:calc_time).first
+    @best_time_21 = CompetitionResult.where(distance: 21).order(:calc_time).first
+    @best_time_42 = CompetitionResult.where(distance: 42).order(:calc_time).first
+    @best_time_100 = CompetitionResult.where(distance: 100).order(:calc_time).first
     @graph_data = current_user.practices.order(:day).limit(10)
     @distance_data = @graph_data.pluck(:day, :distance)
     @weight_data = @graph_data.pluck(:day, :weight)
@@ -15,7 +15,6 @@ class MypagesController < ApplicationController
     @competition_results_all = current_user.competition_results
     #GoogleMap表示用データ取得
     @map = CompetitionResult.set_map(@competition_results_all)
-    #マイペ0時の大会結果一覧表示用に直近３件のみ取得
     @competition_results = @competition_results_all.order(:day).limit(5)
   end
 end
