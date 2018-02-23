194179730802f133e9ae7ee2206ffd8f829bb204
森田啓太 <mancheunited7>
2018-02-23 22:08:26 +0900

    情報ウィンドウ表示

--- app/models/competition_result.rb
+++ app/models/competition_result.rb
@@ -7,11 +7,12 @@ class CompetitionResult < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: Proc.new { |a| a.address_changed? }
 
-  #GoogleMap表示のデータ
+  #GoogleMap表示用データ
   def self.set_map(competition_result)
     hash = Gmaps4rails.build_markers(competition_result) do |competition_result, marker|
       marker.lat competition_result.latitude
       marker.lng competition_result.longitude
+      marker.infowindow competition_result.competition_name
     end
   end
 
